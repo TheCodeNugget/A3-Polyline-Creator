d85ccc76b5c4b2c3cdea01a10ec6eae3a6e53883
TheCodeNugget <kyilmaz190300@gmail.com>
2023-07-16 23:13:27 +0300

    feat: Rewrite using new method

--- roads.py
+++ roads.py
@@ -1,39 +1,69 @@
 import argparse
 from pathlib import Path
 import shapefile
+import math
+import numpy as np
 
-parser = argparse.ArgumentParser(description = "Simple Python Script to remove duplicates from Terrain Builder Object Files")
-parser.add_argument("roads", type=str, help="Path of the Objects File")
-parser.add_argument("ID", type=int, help="Road Type")
-parser.add_argument("-o", "--output", type=str, default="output", help="Name of the output file")
-args = parser.parse_args()
 
-road_path = Path(args.roads)
-assert road_path.exists(), f"Object Files {args.roads} do not exist"
+def closest_node(node, nodes):
+    nodes = np.asarray(nodes)
+    deltas = nodes - node
+    dist_2 = np.einsum('ij,ij->i', deltas, deltas)
+    return np.argmin(dist_2)
 
-f = open(road_path, "r")
-points = eval(f.read())
+limiters = []
+f = open("highwayMarks.txt","r")
+for line in f:
+    lineItems = line.split(";")
+    limiters.append([float(lineItems[1]), float(lineItems[2])])
 f.close()
 
-w = shapefile.Writer(args.output, shapeType=3)
+points = []
+f = open("Highway.txt","r")
+for line in f:
+    lineItems = line.split(";")
+    points.append([float(lineItems[1]), float(lineItems[2])])
+f.close()
+
+w = shapefile.Writer("out.shp", shapeType=3)
 w.field('ID', 'N')
 w.field('ORDER', 'N')
+        
+while (len(limiters) > 1) and (len(points) > 1):
+    currentLine = []
+    currentPoint = limiters[0]
+    limiterHold = limiters[0]
+    limiters.pop(0)
+    currentLine.append(currentPoint)
 
-lines = []
-out = []
-temp = []
-order = 0
+    while (True):
+        closestLimiterIndex = closest_node(currentPoint, limiters)
+        closestLimiter = limiters[closestLimiterIndex]
+        closestLimiterDist = math.dist(currentPoint, closestLimiter)
+        closestPointIndex = closest_node(currentPoint, points)
+        closestPoint = points[closestPointIndex]
+        closestPointDist = math.dist(currentPoint, closestPoint)
+    
+        if ((closestPointDist) > closestLimiterDist):
+            break
+        
+        currentLine.append(closestPoint)
+        currentPoint = closestPoint
+        points.pop(closestPointIndex)
+        
+        if (len(points) == 0):
+            break
+    
+    if (len(currentLine) == 1):
+        continue
+        
+    currentLine.append(closestLimiter)
+    #limiters.pop(closestLimiterIndex)
+    limiters.append(limiterHold)
+    w.line([currentLine])
+    w.record(1,0)
+    
+    if (len(points) == 0):
+        break
 
-lines.append(points[0][4])
-for i in points:
-    if i[4] not in lines:
-        w.line([temp])
-        w.record(args.ID,0)
-        order += 1
-        temp = []
-        lines.append(i[4])
-    temp.append([float(i[1][0])+200000,float(i[1][1])])
-
-w.line([temp])
-w.record(args.ID,order)
 w.close()
